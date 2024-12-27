b90b89fddaa66fcb1bf38af0c40308d9968924d0
Tausif Ahmed <120933222+AR-Tausif@users.noreply.github.com>
2024-12-27 23:44:14 +0600

    changes something

--- src/api/job/job.controller.ts
+++ src/api/job/job.controller.ts
@@ -28,6 +28,30 @@ const CREATE_JOB_INTO_DB = handleAsync(async (req, res) => {
   });
 });
 
+const GET_ALL_JOBS_FROM_DB = handleAsync(async (req, res) => {
+  const { userId } = req.user;
+  const { company, title, description, salaryRange, address, status } =
+    req.body.data;
+
+  // creating job post
+  const result = await db.insert(jobs).values({
+    company,
+    title,
+    createdBy: Number(userId),
+    description,
+    salaryRange,
+    address,
+  });
+
+  // sending response to client
+  formatedResponse(res, {
+    statusCode: HttpStatus.OK,
+    data: result,
+    message: "job post created!",
+  });
+});
+
+
 export const JobController = {
   CREATE_JOB_INTO_DB,
 };
